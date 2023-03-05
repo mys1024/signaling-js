04fe854f074602604b800ecb290be29b21dc6ab2
mys1024 <mys.1024@outlook.com>
2023-03-05 18:32:47 +0800

    chore: update examples

--- examples/basic.ts
+++ examples/basic.ts
@@ -1,24 +1,22 @@
 import { SignalingPeer } from '../src/node'
 
-console.log(123)
-
 async function example() {
   const peer1 = new SignalingPeer('ws://127.0.0.1/ws')
   const pid1 = await peer1.getPid()
-  console.log('peer1:', pid1)
+  console.log('peer1 pid:', pid1)
   peer1.addDataListener((data, from) => {
-    console.log('peer1 recv:', from, data)
+    console.log(`peer1 recv from ${from}:`, data)
   })
 
   const peer2 = new SignalingPeer('ws://127.0.0.1/ws')
   const pid2 = await peer2.getPid()
-  console.log('peer2:', pid2)
+  console.log('peer2 pid:', pid2)
   peer2.addDataListener((data, from) => {
-    console.log('peer2 recv:', from, data)
+    console.log(`peer2 recv from ${from}:`, data)
   })
 
-  console.log(await peer1.send(pid2, { name: 'Li', time: new Date() }))
-  console.log(await peer2.send(pid1, true))
+  console.log('res to peer1:', await peer1.send(pid2, { name: 'Li', time: new Date() }))
+  console.log('res to peer2', await peer2.send(pid1, 'Hi!'))
 }
 
 example()
